ca5a1dd9ceb67a6188286e006bcfbdda905cf7dd
Shio703 <shiodjamurashvili26@gmail.com>
2023-12-05 14:59:38 +0400

    other-pages-added

--- src/app/htmlcss/page.jsx
+++ src/app/htmlcss/page.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 function HtmlCss() {
   return (
     <main className={styles.main}>
-      <h2 className={styles.mainTitle}>This is my 1nd module final project</h2>
+      <h2 className={styles.mainTitle}>HTML & CSS</h2>
+      <section className={styles.mainSec}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/fm.png"
+            width={400}
+            height={350}
+            className={styles.msImage}
+          ></Image>
+        </div>
+        <div className={styles.textWrapper}>
+          <h3>My 1nd module final project</h3>
+          <p>
+            Project "Fake Mess" is a clone of the Corporation Meta's
+            Messenger.com's login page.
+          </p>
+          <span>
+            <a href="https://fakemessenger.netlify.app" target="_blank">
+              see it in live from here
+            </a>
+          </span>
+        </div>
+      </section>
     </main>
   );
 }
--- src/app/javascript/page.jsx
+++ src/app/javascript/page.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 function JavaScript() {
   return (
-    <div>This is my 2nd module final project</div>
-  )
+    <main className={styles.main}>
+      <h2 className={styles.mainTitle}>Javascript</h2>
+      <section className={styles.mainSec}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/fmj.png"
+            width={400}
+            height={350}
+            className={styles.msImage}
+          ></Image>
+        </div>
+        <div className={styles.textWrapper}>
+          <h3>My 2nd module final project</h3>
+          <p>
+            The "FMJavascript" project is an updated version of "Fake Mess". In
+            this update, I added a single page called "Users" where you can view
+            the user information fetched from the API with the help of fetch();.
+          </p>
+          <span>
+            <a href="https://fmjavascript.netlify.app" target="_blank">
+              see it in live from here
+            </a>
+          </span>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default JavaScript
+export default JavaScript;

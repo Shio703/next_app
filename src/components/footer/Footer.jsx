3da35bd7c4b88ab814aa6119090aa285775c9856
Shio703 <shiodjamurashvili26@gmail.com>
2023-12-06 22:04:15 +0400

    upgraded-footer-links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,50 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 function footer() {
   return (
     <footer>
       <div className={styles.footerDiv}>
-        <a className={styles.link} href="mailto:contact.shiotv@gmail.com">
+        <Link className={styles.link} href="mailto:contact.shiotv@gmail.com">
           <Image src="/Gmail-icon.png" width={35} height={35} alt="G-logo" />
-          {/* <img
-            src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/256/Gmail-icon.png"
-            width="35em"/> */}
           Gmail
-        </a>
-        <a
+        </Link>
+        <Link
           className={styles.link}
           href="https://www.instagram.com/shio_zhamurashvili"
-          target="_blank">
+          target="_blank"
+        >
           <Image
             src="/Instagram-icon.png"
             width={35}
             height={35}
-            alt="I-logo"/>
-          {/* <img
-            src="https://icons.iconarchive.com/icons/ionic/ionicons/256/logo-instagram-icon.png"
-            width="35em"
-          /> */}
+            alt="I-logo"
+            className={styles.linkIcon}
+          />
           Instagram
-        </a>
+        </Link>
+        <Link className={styles.link} href="facebook.com">
+          <Image
+            src="/Facebook-icon.png"
+            width={35}
+            height={35}
+            alt="F-Logo"
+            className={styles.linkIcon}
+          />
+          Facebook
+        </Link>
+        <Link className={styles.link} href="twitter.com">
+          <Image
+            src="/Twitter-icon.png"
+            width={35}
+            height={35}
+            alt="X-Logo"
+            className={styles.linkIcon}
+          />
+          Twitter
+        </Link>
         <div className={styles.rightDiv}>@Created By Shio&lt;703/&gt;</div>
       </div>
       <div className={styles.bottomDiv}></div>
